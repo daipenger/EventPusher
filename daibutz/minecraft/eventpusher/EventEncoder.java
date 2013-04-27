3e0f5b69213c8ad767dc1d3bee36e751f3a9cd17
daibutz <papierfliegerfalter@gmail.com>
2013-04-27 12:51:20 +0200

    Initial EventEncoder.java implementation.

---
+++ daibutz/minecraft/eventpusher/EventEncoder.java
@@ -0,0 +1,25 @@
+package daibutz.minecraft.eventpusher;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class EventEncoder implements Listener {
+	
+	private EventPusher eventPusher;
+	
+	public EventEncoder(EventPusher eventPusher) {
+		this.eventPusher = eventPusher;
+	}
+
+	/* Event Handlers */
+	
+	@EventHandler(priority = EventPriority.LOW)
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		eventPusher.push("{\"event\":" + e.getEventName() + "," 
+				+ "\"player\"" + e.getPlayer().getName() + ","
+				+ "\"join-message\"" + e.getJoinMessage()
+				+ "}");
+	}
+}
--- daibutz/minecraft/eventpusher/EventPusher.java
+++ daibutz/minecraft/eventpusher/EventPusher.java
@@ -1,19 +1,29 @@
 package daibutz.minecraft.eventpusher;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EventPusher extends JavaPlugin {
 	
 	private Server server;
+	private EventEncoder eventEncoder;
 	
 	@Override
 	public void onEnable() {
 		server = new Server();
 		server.startListening();
+		
+		eventEncoder = new EventEncoder(this);
+		this.getServer().getPluginManager().registerEvents(eventEncoder, this);
 	}
 	
 	@Override
 	public void onDisable() {
+		HandlerList.unregisterAll(eventEncoder);
 		server.stopListening();
+	}
+	
+	public void push(String jsonMessage) {
+		server.sendToAllClients(jsonMessage);
 	}
 }
