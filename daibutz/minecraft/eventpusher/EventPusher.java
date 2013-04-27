f9d45a47491d33ea21d30862878fb7403e93d183
daibutz <papierfliegerfalter@gmail.com>
2013-04-27 08:01:46 +0200

    Initial commit.

---
+++ daibutz/minecraft/eventpusher/EventPusher.java
@@ -0,0 +1,12 @@
+package daibutz.minecraft.eventpusher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventPusher extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+}
