98e14e89a11df728ddac0f5c97eead8e51a80963
Fabian Bauer <fobbes@web.de>
2014-06-15 22:55:33 +0200

    Remove PlayerVelocityEvent and add BlockBreakEvent

--- daibutz/minecraft/eventpusher/EventEncoder.java
+++ daibutz/minecraft/eventpusher/EventEncoder.java
@@ -1,29 +1,29 @@
 package daibutz.minecraft.eventpusher;
 
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonArray;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonObject;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerVelocityEvent;
-import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonArray;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonObject;
 
 public class EventEncoder implements Listener {
-	
+
 	private EventPusher eventPusher;
-	
+
 	public EventEncoder(EventPusher eventPusher) {
 		this.eventPusher = eventPusher;
 	}
-	
+
 	/* Helpers */
-	
+
 	public static JsonObject encodePlayer(Player p) {
 		JsonObject j = new JsonObject();
 		j.addProperty("name", p.getName());
@@ -33,81 +33,81 @@ public class EventEncoder implements Listener {
 		j.addProperty("level", p.getLevel());
 		return j;
 	}
-	
+
 	public static JsonObject encodeBlock(Block b) {
 		JsonObject j = new JsonObject();
 		j.addProperty("x", b.getX());
 		j.addProperty("y", b.getY());
 		j.addProperty("z", b.getZ());
-		
-		j.addProperty("type", b.getType().toString());
+		j.addProperty("type", b.getType().name());
 		return j;
 	}
 
 	/* Event Handlers */
-	
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerJoin(PlayerJoinEvent e) {
 		JsonObject j = new JsonObject();
 		j.addProperty("event", e.getEventName());
 		j.add("player", encodePlayer(e.getPlayer()));
 		j.addProperty("message", e.getJoinMessage());
-		
+
 		eventPusher.push(j.toString());
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerQuit(PlayerQuitEvent e) {
 		JsonObject j = new JsonObject();
 		j.addProperty("event", e.getEventName());
 		j.add("player", encodePlayer(e.getPlayer()));
 		j.addProperty("message", e.getQuitMessage());
-		
+
 		eventPusher.push(j.toString());
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerChat(AsyncPlayerChatEvent e) {
 		JsonObject j = new JsonObject();
 		j.addProperty("event", e.getEventName());
 		j.add("player", encodePlayer(e.getPlayer()));
 		JsonArray recipients = new JsonArray();
-		for (Player p : e.getRecipients()) 
+		for (Player p : e.getRecipients())
 			recipients.add(encodePlayer(p));
 		j.add("recipients", recipients);
 		j.addProperty("format", e.getFormat());
 		j.addProperty("message", e.getMessage());
-		
+
 		eventPusher.push(j.toString());
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onBlockPlace(BlockPlaceEvent e) {
 		JsonObject j = new JsonObject();
 		j.addProperty("event", e.getEventName());
 		j.add("player", encodePlayer(e.getPlayer()));
 		j.add("block", encodeBlock(e.getBlock()));
-		
+
 		eventPusher.push(j.toString());
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onPlayerVelocity(PlayerVelocityEvent e) {
+	public void onBlockBreak(BlockBreakEvent e) {
 		JsonObject j = new JsonObject();
 		j.addProperty("event", e.getEventName());
 		j.add("player", encodePlayer(e.getPlayer()));
-		j.addProperty("velocity", e.getVelocity().toString());
-		
+		j.add("block", encodeBlock(e.getBlock()));
+
 		eventPusher.push(j.toString());
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerDeath(PlayerDeathEvent e) {
 		JsonObject j = new JsonObject();
 		j.addProperty("event", e.getEventName());
 		j.add("player", encodePlayer(e.getEntity()));
 		j.addProperty("message", e.getDeathMessage());
-		
+
 		eventPusher.push(j.toString());
 	}
+
 }
