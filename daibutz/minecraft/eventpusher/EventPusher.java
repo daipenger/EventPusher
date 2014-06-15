9b554c1f23399661dc124013b50aa15947925352
Fabian Bauer <fobbes@web.de>
2014-06-15 22:59:40 +0200

    Make config file useable

--- daibutz/minecraft/eventpusher/EventPusher.java
+++ daibutz/minecraft/eventpusher/EventPusher.java
@@ -1,28 +1,35 @@
 package daibutz.minecraft.eventpusher;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EventPusher extends JavaPlugin {
-	
+
 	private Server server;
 	private EventEncoder eventEncoder;
-	
+
+	public EventPusher() {
+		// Save the predefined and default config into the plugin folder if it is not there
+		saveDefaultConfig();
+
+		FileConfiguration config = super.getConfig();
+		server = new Server(config.getInt("port", 7112), config.getInt("max-clients", 1));
+		eventEncoder = new EventEncoder(this);
+	}
+
 	@Override
 	public void onEnable() {
-		server = new Server(getConfig().getInt("port"), getConfig().getInt("max-clients"));
 		server.startListening();
-		
-		eventEncoder = new EventEncoder(this);
-		this.getServer().getPluginManager().registerEvents(eventEncoder, this);
+		super.getServer().getPluginManager().registerEvents(eventEncoder, this);
 	}
-	
+
 	@Override
 	public void onDisable() {
 		HandlerList.unregisterAll(eventEncoder);
 		server.stopListening();
 	}
-	
+
 	public void push(String jsonMessage) {
 		if (getConfig().getBoolean("debug.print-to-console"))
 			getLogger().info(jsonMessage);
