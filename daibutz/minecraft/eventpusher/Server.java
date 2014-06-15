8cc447df61452c2de6477ac147f0c6d2976b55f4
Fabian Bauer <fobbes@web.de>
2014-06-15 23:02:47 +0200

    Fixed Exception at sendToAllClients

--- daibutz/minecraft/eventpusher/Server.java
+++ daibutz/minecraft/eventpusher/Server.java
@@ -5,64 +5,66 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.Vector;
 
 public class Server {
 
 	private int port;
 	private int maxClients;
-	
+
 	private ServerSocket serverSocket;
 	private Vector<BufferedWriter> clients;
-	
+
 	public Server(int port, int maxClients) {
 		this.port = port;
 		this.maxClients = maxClients;
 		clients = new Vector<BufferedWriter>(maxClients);
 	}
-	
+
 	/**
 	 * Start listening for and manage incoming connections.
 	 */
 	public void startListening() {
 		try {
 			serverSocket = new ServerSocket(port);
-			
+
 			new Thread(new Runnable() {
 				@Override
 				public void run() {
 					while (true) {
 						try {
-							// Listen for new incoming connections and add them to the list of clients.
-							// This will block the thread. Calling serverSocket.close() will cause an
+							// Listen for new incoming connections and add them
+							// to the list of clients.
+							// This will block the thread. Calling
+							// serverSocket.close() will cause an
 							// IOException.
 							Socket newSocket = serverSocket.accept();
-							
+
 							if (clients.size() >= maxClients) {
 								newSocket.close();
 								continue;
 							}
-							
-							// Reading from clients is not needed for now. It's sufficient to keep track
+
+							// Reading from clients is not needed for now. It's
+							// sufficient to keep track
 							// of the writers.
-							BufferedWriter clientWriter = new BufferedWriter(
-									new OutputStreamWriter(newSocket.getOutputStream()));
-							
+							BufferedWriter clientWriter = new BufferedWriter(new OutputStreamWriter(newSocket.getOutputStream()));
+
 							clients.add(clientWriter);
-							
+
 						} catch (IOException e) {
 							break;
 						}
 					}
 				}
 			}).start();
-			
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Stop listening and do some clean-up.
 	 */
@@ -71,31 +73,31 @@ public class Server {
 			try {
 				serverSocket.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
+
 		serverSocket = null;
-		
+
 		clients.clear();
 	}
-	
+
 	/**
 	 * Sends a string message to all currently connected clients.
 	 * 
-	 * @param message The message.
+	 * @param message
+	 *            The message.
 	 */
-	
 	public void sendToAllClients(String message) {
-		for (BufferedWriter client : clients) {
+		Iterator<BufferedWriter> iterator = clients.iterator();
+		while (iterator.hasNext()) {
+			BufferedWriter client = iterator.next();
 			try {
 				client.write(message);
 				client.newLine();
 				client.flush();
-				
 			} catch (IOException e) {
 				// Assume the remote connection has closed.
-				clients.remove(client);
+				iterator.remove();
 			}
 		}
 	}
